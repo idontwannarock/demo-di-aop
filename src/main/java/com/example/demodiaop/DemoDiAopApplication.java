045e72b05fc58f7caead8439e5ba0ba2a83ac082
Howard Wang <idontwannarock@gmail.com>
2022-11-21 14:27:49 +0800

    ci: add Spring Boot support

---
+++ src/main/java/com/example/demodiaop/DemoDiAopApplication.java
@@ -0,0 +1,12 @@
+package com.example.demodiaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDiAopApplication.class, args);
+    }
+}
